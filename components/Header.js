0c33592d25da2686561ef7cd9735df0a86fef43f
pisanabravetta-pixel <pisanabravetta@gmail.com>
2025-12-31 22:14:01 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
       <div className="container mx-auto px-4 py-4">
         <div className="flex items-center justify-between">
           <div className="flex items-center">
-            <img src="/logo.png" alt="ServiziSalute Logo" className="h-10 w-auto mr-3" />
+           
             <h1 className="text-2xl font-bold text-primary">ServiziSalute</h1>
           </div>
           <nav>
@@ -22,4 +22,5 @@ export default function Header() {
       </div>
     </header>
   );
+
 }
