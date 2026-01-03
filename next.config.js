47414ceea84a437db20394aba834b286374479f0
pisanabravetta-pixel <pisanabravetta@gmail.com>
2026-01-03 16:52:09 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['localhost', 'servizisalute.it'],
-  },
-  env: {
-    API_URL: process.env.API_URL || 'http://localhost:3000/api',
-  },
 }
 
 module.exports = nextConfig
