90d31964230d70469202c619faf1ebe17b7771cf
pisanabravetta-pixel <pisanabravetta@gmail.com>
2026-01-03 16:49:02 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        'primary': '#2c843b',
-        'secondary': '#007bff',
-        'light': '#f8f9fa',
-        'dark': '#333333',
-      },
-      fontFamily: {
-        'sans': ['Roboto', 'Arial', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
